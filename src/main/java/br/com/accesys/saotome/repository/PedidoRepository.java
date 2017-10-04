7db638ce82d087ca9587a51471ab3d7737effd1a
Ander Parra <ander.pp@gmail.com>
2017-10-04 23:27:46 +0200

    removendo comentarios desnecessarios

--- src/main/java/br/com/accesys/saotome/repository/PedidoRepository.java
+++ src/main/java/br/com/accesys/saotome/repository/PedidoRepository.java
@@ -10,10 +10,6 @@ import org.springframework.data.jpa.repository.*;
 
 import java.util.List;
 
-
-/**
- * Spring Data JPA repository for the Pedido entity.
- */
 @SuppressWarnings("unused")
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
