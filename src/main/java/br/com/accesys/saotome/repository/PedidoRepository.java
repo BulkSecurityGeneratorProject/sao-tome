3b87214e2a965394cb7fdfa2154801a74b30162e
Ander Parra <ander.pp@gmail.com>
2017-10-04 23:24:55 +0200

    buscar pedidos por comanda

--- src/main/java/br/com/accesys/saotome/repository/PedidoRepository.java
+++ src/main/java/br/com/accesys/saotome/repository/PedidoRepository.java
@@ -1,9 +1,14 @@
 package br.com.accesys.saotome.repository;
 
+import br.com.accesys.saotome.domain.Comanda;
 import br.com.accesys.saotome.domain.Pedido;
+import br.com.accesys.saotome.domain.Produto;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
+
+import java.util.List;
 
 
 /**
@@ -13,4 +18,7 @@ import org.springframework.data.jpa.repository.*;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+    default List<Pedido> buscarPorComanda(Comanda comanda) {
+        return findAll(Example.of(new Pedido().comanda(comanda)));
+    }
 }
